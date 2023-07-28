9e881283b40cb9ef43a8d0f65c290ca99a75657c
Jonathan Ang <j.ang2@yahoo.com>
2023-07-27 21:12:48 -0700

    get all channels by server id

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,5 +1,72 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import Server
+from app.models import Server, Channel
 
 server_routes = Blueprint('servers', __name__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Getting all the channels of a server
+@server_routes.route("/<int:id>/channels")
+@login_required
+def get_all_channels_of_a_server(id):
+    """
+    Query for all the channels by a server id
+    """
+    server = Server.query.get(id)
+
+    if server is None:
+        return {}
+
+    all_channels = {"Channels": [channels.to_dict() for channels in server.channels]}
+    return all_channels
+
+
