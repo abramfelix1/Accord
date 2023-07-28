667b1d2d0b29752b78bc6f44795882b8e88aa6ef
randy hac <randydhacks@gmail.com>
2023-07-28 02:33:14 -0700

    working on it

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
-from flask_login import LoginManager
-from .models import db, User
+from flask_login import LoginManager, login_required, current_user
+from .models import db, User, Server, Member
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.server_routes import server_routes
@@ -97,6 +97,24 @@ def react_root(path):
     if path == "favicon.ico":
         return app.send_from_directory("public", "favicon.ico")
     return app.send_static_file("index.html")
+
+
+@app.route('/api/server/<int:id>/join', methods=["GET"])
+@login_required
+def join_server(id):
+
+    # Get Server Id
+    server_id = Server.query.get(id).id
+    # Get current user Id
+    user_id = current_user.get_id()
+
+    member = Member.query\
+        .join(User)\
+        .join(Server)\
+        .filter_by(user_id=user_id, server_id=server_id).first()
+
+    print(member)
+    return member.to_dict()
 
 
 @app.errorhandler(404)
