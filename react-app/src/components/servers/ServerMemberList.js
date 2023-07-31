6b25dd76f535391f314f9c1c6b63103a7e0e1475
Jonathan Ang <j.ang2@yahoo.com>
2023-07-31 01:36:37 -0700

    got server members to show

--- react-app/src/components/servers/ServerMemberList.js
+++ react-app/src/components/servers/ServerMemberList.js
@@ -5,26 +5,26 @@ import { useEffect, useState } from "react";
 function ServerMemberList({ server }) {
     const dispatch = useDispatch()
     const serverMembers = Object.values(useSelector((state) => state.members));
-    
+
     useEffect(() => {
         (async () => {
-            // currently just setting the server 1 as a starting point 
+            // currently just setting the server 1 as a starting point until
             // we can figure out what to set the list starting point to
             // when you are not on a server yet.
             await dispatch(memberActions.getServerMembersThunk(server.id || 1));
         })();
-    }, [dispatch]);
+    }, [dispatch, server]);
+
 
     return (
         <div style={{backgroundColor: '#2B2D30', width: '14.938rem', color: 'white'}}>
             MemberList
             <ul>
-                {serverMembers.map((member) => {
+                {serverMembers.map((member) => (
                     <li key={member.id}>
                         <p>{member.display_name}</p>
                     </li>
-                })}
-                hello
+                ))}
             </ul>
         </div>
     )
