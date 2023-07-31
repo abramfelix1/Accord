1b598a3fe675bf48e1fba99c85a6034f8adf9309
Abram Felix <abramfelix1@gmail.com>
2023-07-30 19:01:28 -0700

    dev to channel-reducer

--- app/models/channel.py
+++ app/models/channel.py
@@ -6,11 +6,13 @@ class Channel(db.Model):
     __tablename__ = "channels"
 
     if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+        __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    server_id = db.Column(db.Integer(), db.ForeignKey(add_prefix_for_prod("servers.id")), nullable=False)
+    server_id = db.Column(
+        db.Integer(), db.ForeignKey(add_prefix_for_prod("servers.id")), nullable=False
+    )
     created_at = db.Column(db.DateTime(), default=datetime.now)
     updated_at = db.Column(db.DateTime(), default=datetime.now)
 
@@ -18,13 +20,17 @@ class Channel(db.Model):
     server = db.relationship("Server", back_populates="channels")
 
     # many to many with users through channel messages tables
-    users = db.relationship("User", secondary="channel_messages", cascade="all,delete", back_populates="channel_messages")
-
+    users = db.relationship(
+        "User",
+        secondary="channel_messages",
+        cascade="all,delete",
+        back_populates="channel_messages",
+    )
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'name': self.name,
+            "id": self.id,
+            "name": self.name,
             "server_id": self.server_id,
             "created_at": self.created_at,
             "updated_at": self.updated_at,
@@ -32,8 +38,8 @@ class Channel(db.Model):
 
     def to_dict_relationships(self):
         return {
-            'id': self.id,
-            'name': self.name,
+            "id": self.id,
+            "name": self.name,
             "server_id": self.server_id,
             "created_at": self.created_at,
             "updated_at": self.updated_at,
