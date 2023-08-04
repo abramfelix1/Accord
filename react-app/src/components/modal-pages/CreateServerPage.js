430b01c1a8babf2a7dc2d4f30347bf0c43077b6d
Jonathan Ang <j.ang2@yahoo.com>
2023-08-03 20:20:47 -0700

    working on server creating form

--- react-app/src/components/modal-pages/CreateServerPage.js
+++ react-app/src/components/modal-pages/CreateServerPage.js
@@ -1,12 +1,14 @@
 import "./model-pages-css/CreateServer.css"
 import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux";
-import { BiSolidCamera } from "react-icons/bi";
+// import { BiSolidCamera } from "react-icons/bi";
 
 
 function CreateServerPage() {
-    const [serverName, setServerName] = useState("")
-    const user = useSelector(state => state.session.user)
+    const dispatch = useDispatch();
+    const [serverName, setServerName] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const user = useSelector(state => state.session.user);
     
     useEffect(() => {
         if(user) {
@@ -14,7 +16,10 @@ function CreateServerPage() {
         }
     }, [user])
 
-    // HiMiniCamera
+    const handleSubmit = async () => {
+        
+    }
+
     return(
         <form className="create-server-container">
             <div className="c-server-top-wrap">
@@ -23,17 +28,32 @@ function CreateServerPage() {
                     Give your new server a personality with a name and
                     an icon. You can always change it later.
                 </p>
-                <div className="c-server-image-outer-wrapper">
+                {/* comment this back in when AWS in provided and comment out image input */}
+                {/* <div className="c-server-image-outer-wrapper">
                     <div className="c-server-image-wrapper">
                         <BiSolidCamera className="c-server-camera"/>
                         <p className="c-server-upload-text">UPLOAD</p>
+                        <p className="c-cerver-plus"><span>+</span></p>
                     </div>
-                </div>
-                <div className="c-server-name">
-                    <label>SERVER NAME</label>
-                    <input type="text" value={serverName}></input>
-                </div>
-                <p className="c-server-agreement">By creating a server, you agree to Accord's Community Guidelines</p>
+                </div> */}
+                <label className="c-server-image-label">IMAGE URL
+                    <input 
+                    className="c-server-image-input" 
+                    type="text" value={imageUrl} 
+                    onChange={e => setImageUrl(e.target.value)} 
+                    />
+                </label>
+                <label className="c-server-name-label">SERVER NAME *
+                    <input 
+                    className="c-server-name-input" 
+                    type="text" value={serverName} 
+                    onChange={e => setServerName(e.target.value)} 
+                    maxLength={100}
+                    />
+                </label>
+                <p className="c-server-agreement">By creating a server, you agree to Accord's  
+                    <span className="c-server-guidelines"> Community Guidelines</span>
+                </p>
             </div>
             <div className="c-server-bottom-wrap">
                 <p className="c-server-back">Back</p>
--- react-app/src/components/servers/ServerCard.js
+++ react-app/src/components/servers/ServerCard.js
@@ -4,7 +4,7 @@
 function ServerCard({ server, handleActiveButton, toolTip, setToolTip }){
 
     // gets the initals of the server name and return them capitalize
-    const initals = (serverName) => {
+    const initials = (serverName) => {
         let res = "";
         const serverNameArr = serverName.split(" ");
 
@@ -12,12 +12,12 @@ function ServerCard({ server, handleActiveButton, toolTip, setToolTip }){
             let word = serverNameArr[i];
             res += word[0].toUpperCase();
         }
-        return res;
+        return res.slice(0, 3);
     } 
 
     return (
         <>
-            {server.image_url !== null && server.image_url.length >= 1
+            {server.image_url !== null && server.image_url.length >= 3
             ? 
             <div className="tooltip">
                 <img className="servers-img" 
@@ -32,7 +32,7 @@ function ServerCard({ server, handleActiveButton, toolTip, setToolTip }){
             <div className="servers tooltip"
                 onClick={e => handleActiveButton(e)}
             >
-                {initals(server.name)}
+                {initials(server.name)}
             </div>}
 
             {toolTip && <span className="tooltiptext">{server.name}</span>}
