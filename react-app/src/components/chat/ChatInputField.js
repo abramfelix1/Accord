b12f1bb2e01c925b095820114a06380a46065d31
Jonathan Ang <j.ang2@yahoo.com>
2023-08-04 02:30:10 -0700

    started working on text input box for chat

--- react-app/src/components/chat/ChatInputField.js
+++ react-app/src/components/chat/ChatInputField.js
@@ -1,8 +1,30 @@
+import { useContext } from "react";
+import { ChannelContext } from "../../context/channelContext";
+import "./chat-css/ChatInputField.css"
+import { FaGift } from 'react-icons/fa';
+import { HiGif } from 'react-icons/hi2';
+import { LuSticker } from 'react-icons/lu';
+import { BsFillEmojiSmileFill } from 'react-icons/bs';
+
+
+
 function ChatInputField({ sendChat, chatInput, updateChatInput }) {
+  
+  const { channel } = useContext(ChannelContext);
+
   return (
-    <form onSubmit={sendChat}>
-      <input value={chatInput} onChange={updateChatInput} />
-      <button type="submit">Send</button>
+    <form id="chat-form-input-container" onSubmit={sendChat}>
+      <textarea
+        id="chat-input" 
+        placeholder={`Message #${channel.name}`} 
+        value={chatInput} 
+        onChange={updateChatInput} 
+      ></textarea>
+      <FaGift className="chat-gift-icon"/>
+      <HiGif className="chat-gif-icon"/>
+      <LuSticker className="chat-sticker-icon"/>
+      <BsFillEmojiSmileFill className="chat-emoji-icon"/>
+      {/* <button type="submit">Send</button> */}
     </form>
   );
 }
