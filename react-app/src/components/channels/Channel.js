b1cad9efcfea6a4638d0d1d6afb4da9f2f482c43
randy hac <randydhacks@gmail.com>
2023-08-04 23:52:59 -0700

    added active channel display

--- react-app/src/components/channels/Channel.js
+++ react-app/src/components/channels/Channel.js
@@ -2,7 +2,7 @@ import { NavLink, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useParams, Redirect, useHistory } from "react-router-dom";
 import * as channelActions from "../../store/channels";
-import { IoIosArrowDown,IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
 import { FaHashtag } from "react-icons/fa";
 import { BiPlus, BiSolidCog } from "react-icons/bi";
 import { useDispatch, useSelector } from "react-redux";
@@ -15,20 +15,27 @@ import { logout } from "../../store/session";
 import "./channel-css/Channel.css";
 
 function Channel({ server }) {
-  // const {id} = useParams()
+  // router doms
   const dispatch = useDispatch();
-  const { setChannel } = useContext(ChannelContext);
-  const { createChannelModal, channelSettingModal } = useContext(ModalContext);
-  const { setChannelCog } = useContext(InfoContext)
-  const [showTextChannel, setShowTextChannel] = useState(true);
-  const isLoaded = useSelector((state) => state.channels.isLoading);
+  const { channelid } = useParams();
   const history = useHistory();
 
+  // useSelectors
+  const isLoaded = useSelector((state) => state.channels.isLoading);
   const user = useSelector((state) => state.session.user);
   const channels = Object.values(
     useSelector((state) => state.channels.channels)
   );
 
+  // Contexts
+  const { setChannel } = useContext(ChannelContext);
+  const { createChannelModal, channelSettingModal } = useContext(ModalContext);
+  const { setChannelCog } = useContext(InfoContext);
+
+  // useStates
+  const [showTextChannel, setShowTextChannel] = useState(true);
+
+  // useEffects
   useEffect(() => {
     if (server) {
       (async () => {
@@ -37,6 +44,7 @@ function Channel({ server }) {
     }
   }, [dispatch, server]);
 
+  // Handlers
   const logoutHandler = async () => {
     await dispatch(logout());
     return history.push("/login");
@@ -53,61 +61,69 @@ function Channel({ server }) {
         <div>
           <ul className="channel-list">
             <li className="channel-list-title-container">
-              <div className='dropdown-text-channel' style={{ display: "flex", alignItems: "center" }}>
-                {showTextChannel ?
-                <IoIosArrowDown className=".text-channel-drop-down-icon" onClick={e => setShowTextChannel(!true)}/>
-                :
-                <IoIosArrowForward className=".text-channel-drop-down-icon" onClick={e => setShowTextChannel(!false)}/>
-                }
+              <div
+                className="dropdown-text-channel"
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                {showTextChannel ? (
+                  <IoIosArrowDown
+                    className=".text-channel-drop-down-icon"
+                    onClick={(e) => setShowTextChannel(!true)}
+                  />
+                ) : (
+                  <IoIosArrowForward
+                    className=".text-channel-drop-down-icon"
+                    onClick={(e) => setShowTextChannel(!false)}
+                  />
+                )}
                 <p className="channel-list-title">Text Channels</p>
               </div>
               {user.id === server.owner_id ? (
                 <BiPlus
                   className="text-channel-add-icon create-new-channel-plus"
                   onClick={(e) => createChannelModal()}
-                />)
-                :
-                (
+                />
+              ) : (
                 <BiPlus
                   className="text-channel-add-icon create-new-channel-plus hide-plus"
                   onClick={(e) => createChannelModal()}
                 />
               )}
             </li>
-            {showTextChannel ?
-
-            <li>
-              {channels.map((channel) => {
-                return (
-                  <div className="channel-box">
-                    <Link
-                      key={channel.id}
-                      to={`/servers/${server.id}/channels/${channel.id}`}
-                      className="channel-flex"
-                      onClick={(e) => {
-                        channelClickHandler(channel);
-                      }}
-                    >
-                      <FaHashtag style={{ color: "#949ba4" }} />
-                      <p className="channel-name">{channel.name}</p>
-                    </Link>
-                    {user.id === server.owner_id && (
-                      <div className="cog-container">
-                        <BiSolidCog
-                          className="channel-cog-settings"
-                          onClick={(e) => {
-                            setChannelCog(channel)
-                            channelSettingModal();
-                          }}
-                        />
+            {showTextChannel ? (
+              <li>
+                {channels.map((channel) => {
+                  return (
+                    <div className={channelid == channel.id ? 'channel-box-active' : 'channel-box'}>
+                      <NavLink
+                        key={channel.id}
+                        to={`/servers/${server.id}/channels/${channel.id}`}
+                        className={'channel-flex'}
+                        onClick={(e) => {
+                          channelClickHandler(channel);
+                        }}
+                      >
+                        <FaHashtag style={{ color: "#949ba4" }} />
+                        <p className="channel-name">{channel.name}</p>
+                      </NavLink>
+                      {user.id === server.owner_id && (
+                        <div className="cog-container">
+                          <BiSolidCog
+                            className="channel-cog-settings"
+                            onClick={(e) => {
+                              setChannelCog(channel);
+                              channelSettingModal();
+                            }}
+                          />
                         </div>
-                    )}
-                  </div>
-                );
-              })}
-            </li>
-            : ''
-}
+                      )}
+                    </div>
+                  );
+                })}
+              </li>
+            ) : (
+              ""
+            )}
             <button onClick={(e) => logoutHandler()}> logout </button>
           </ul>
         </div>
