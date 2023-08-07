612dd5a35de16439fa5e3676156e547f403215ed
Abram Felix <abramfelix1@gmail.com>
2023-08-07 08:05:47 -0700

    added mesasge hover buttons and effects

--- react-app/src/components/chat/Chat.js
+++ react-app/src/components/chat/Chat.js
@@ -11,6 +11,7 @@ import { useRef } from "react";
 import logo from "../../images/accord-logo.png";
 import ChatLoading from "../loading/ChatLoading";
 import { InfoContext } from "../../context/infoContext";
+import MessageContainer from "./MessageContainer";
 
 const Chat = () => {
   const [chatInput, setChatInput] = useState("");
@@ -97,33 +98,35 @@ const Chat = () => {
           <div className="chat-container">
             {reverseArray([...messages]).map((message, idx) => (
               <div key={`${message.id}${idx}`}>
-                <div className="message-wrapper">
-                  {message.image_url !== null &&
-                  message.image_url.length >= 1 ? (
-                    <img
-                      className="chatbox-image"
-                      src={message.image_url}
-                      alt="chatbox-user-img"
-                    />
-                  ) : (
-                    <div className="chatbox-logo-wrapper">
-                      <img className="chatbox-logo" src={logo} alt="logo" />
+                <MessageContainer>
+                  <div className="message-wrapper">
+                    {message.image_url !== null &&
+                    message.image_url.length >= 1 ? (
+                      <img
+                        className="chatbox-image"
+                        src={message.image_url}
+                        alt="chatbox-user-img"
+                      />
+                    ) : (
+                      <div className="chatbox-logo-wrapper">
+                        <img className="chatbox-logo" src={logo} alt="logo" />
+                      </div>
+                    )}
+                    <div className="chat-box-name-date-message-wrapper">
+                      <div className="chat-box-name-date-wrapper">
+                        <span className="chat-box-name">
+                          {message.display_name
+                            ? message.display_name
+                            : message.username}
+                        </span>
+                        <span className="chat-box-date">
+                          {dateFormat(message.created_at)}
+                        </span>
+                      </div>
+                      <p className="chat-box-message">{message.message}</p>
                     </div>
-                  )}
-                  <div className="chat-box-name-date-message-wrapper">
-                    <div className="chat-box-name-date-wrapper">
-                      <span className="chat-box-name">
-                        {message.display_name
-                          ? message.display_name
-                          : message.username}
-                      </span>
-                      <span className="chat-box-date">
-                        {dateFormat(message.created_at)}
-                      </span>
-                    </div>
-                    <p className="chat-box-message">{message.message}</p>
                   </div>
-                </div>
+                </MessageContainer>
               </div>
             ))}
             {/* <div className="message-ref" ref={messageRef}></div> */}
--- react-app/src/components/chat/MessageContainer.js
+++ react-app/src/components/chat/MessageContainer.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import "./chat-css/message.css";
+import { BsFillEmojiSmileFill } from "react-icons/bs";
+import { MdOutlineModeEditOutline } from "react-icons/md";
+import { BsThreeDots } from "react-icons/bs";
 
-function MessageContainer({ sendChat, chatInput, updateChatInput }) {
-  return <div></div>;
+function MessageContainer({ children }) {
+  const [showSettings, setShowSettings] = useState(false);
+
+  return (
+    <div
+      className="message-container"
+      onMouseEnter={() => setShowSettings(true)}
+      onMouseLeave={() => setShowSettings(false)}
+    >
+      {showSettings && (
+        <div className="message-settings-container">
+          <div className="message-icon-container emoji">
+            <BsFillEmojiSmileFill className="message-emoji" />
+          </div>
+          <div className="message-icon-container edit">
+            <MdOutlineModeEditOutline className="message-edit" />
+          </div>
+          <div className="message-icon-container more">
+            <BsThreeDots className="message-more" />
+          </div>
+        </div>
+      )}
+      {children}
+    </div>
+  );
 }
 
 export default MessageContainer;
