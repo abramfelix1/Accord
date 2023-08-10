66dfd4de15d94a91a4d166e466965adf435ab780
Jonathan Ang <j.ang2@yahoo.com>
2023-08-10 01:28:30 -0700

    fixed serverlist images with dead links

--- react-app/src/components/servers/ServerCard.js
+++ react-app/src/components/servers/ServerCard.js
@@ -48,9 +48,9 @@ function ServerCard({ handleActiveButton, serverInfo }) {
                 className={`servers-img ${
                   serverInfo.id == serverid ? "active-server-img" : ""
                 }`}
-                src={server.image_url}
+                src={serverInfo.image_url}
                 alt="serverimage"
-                // onClick={(e) => handleActiveButton(e)}
+
               />
             </div>
           ) : (
@@ -70,87 +70,3 @@ function ServerCard({ handleActiveButton, serverInfo }) {
   );
 }
 export default ServerCard;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { NavLink } from "react-router-dom";
-// import { useParams } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import { useContext, useEffect } from "react";
-// import { InfoContext, InfoProvider } from "../../context/infoContext";
-
-
-// function ServerCard({ handleActiveButton, serverInfo }) {
-//   const { serverid } = useParams();
-//   // const { server } = useContext(InfoContext);
-//   const server = useSelector((state) => state.current.server);
-
-//   useEffect(() => {}, [serverid]);
-
-//   // gets the initals of the server name and return them capitalize
-//   const initials = (serverName) => {
-//     let res = "";
-//     const serverNameArr = serverName.split(" ");
-
-//     for (let i = 0; i < serverNameArr.length; i++) {
-//       let word = serverNameArr[i];
-//       // console.log(typeof word === "string", "---------------")
-//       if(word && typeof word === "string") {
-//         res += word[0].toUpperCase();
-//       }
-//     }
-
-//     if (res.length >= 3) {
-//       return res.slice(0, 3);
-//     }
-
-//     return res;
-//   };
-
-//   return (
-//     <>
-//       {server && (
-//         <NavLink
-//           to={`/servers/${serverInfo.id}/channels/${serverInfo.firstChannel.id}`}
-//           className={`servers server-pointer ${
-//             serverInfo.id == serverid ? "active-server" : ""
-//           }`}
-//           onClick={(e) => handleActiveButton(e, serverInfo)}
-//         >
-//           {serverInfo.image_url !== null && serverInfo.image_url.length >= 3 ? (
-//             <div>
-//               <img
-//                 className="servers-img"
-//                 src={server.image_url}
-//                 alt="serverimage"
-//                 onClick={(e) => handleActiveButton(e)}
-//               />
-//             </div>
-//           ) : (
-//             <div
-//               className="server-initials"
-//               onClick={(e) => {
-//                 // e.stopPropagation();
-//                 e.preventDefault();
-//               }}
-//             >
-//               {initials(serverInfo.name)}
-//             </div>
-//           )}
-//         </NavLink>
-//       )}
-//     </>
-//   );
-// }
-// export default ServerCard;
