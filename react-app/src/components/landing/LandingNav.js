8f8b7b9fd0bc28128023075db39adc2ad01b153e
Jonathan Ang <j.ang2@yahoo.com>
2023-08-11 20:35:20 -0700

    fixed over flow on showing joining all servers when text is one long name

--- react-app/src/components/landing/LandingNav.js
+++ react-app/src/components/landing/LandingNav.js
@@ -1,8 +1,11 @@
 import logo from "../../images/accord-logo.png";
 import { NavLink } from "react-router-dom";
 import "./landing-css/LandingNav.css";
+import { useSelector} from "react-redux";
 
 function LandingNavBar() {
+  const user = useSelector((state) => state.session.user);
+
   return (
     <div className="nav-outer-container">
       <div className="nav-inner-container">
@@ -23,7 +26,7 @@ function LandingNavBar() {
         </nav> */}
         <div className="nav-login-wrapper">
           <a className="landing-login-button" href="/login">
-            Log in
+            {user ? "Open Accord" : "Log in"}
           </a>
         </div>
       </div>
