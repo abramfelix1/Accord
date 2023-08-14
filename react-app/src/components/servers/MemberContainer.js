6aaccd797077c3cb1925dff4b5ac7b24af4248cf
Abram Felix <abramfelix1@gmail.com>
2023-08-13 17:11:13 -0700

    center profile pic

--- react-app/src/components/servers/MemberContainer.js
+++ react-app/src/components/servers/MemberContainer.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 
 function MemberContainer({ children }) {
+  const [width, setWidth] = useState("");
+
   return <div className="member-parent-container">{children}</div>;
 }
 
