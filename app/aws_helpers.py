359902f67dea2f8371ee780d5d04658b2251b1dc
Abram Felix <abramfelix1@gmail.com>
2023-08-13 21:07:16 -0700

    turn aws helpers back on

--- app/aws_helpers.py
+++ app/aws_helpers.py
@@ -1,6 +1,6 @@
 import eventlet
 
-# eventlet.monkey_patch()
+eventlet.monkey_patch()
 
 import boto3
 from boto3.s3.transfer import TransferConfig
@@ -11,7 +11,7 @@ import uuid
 BUCKET_NAME = os.environ.get("S3_BUCKET")
 S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
 ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif", "svg"}
-# CONFIG = TransferConfig(use_threads=False)
+CONFIG = TransferConfig(use_threads=False)
 
 # s3 = boto3.client("s3")
 s3 = boto3.client(
